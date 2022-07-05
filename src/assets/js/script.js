a526d9411a5abd0de01bd9890176072f363ddd8a
eduardob1609 <fraedu3@gmail.com>
2022-07-04 22:33:39 -0300

    finalizando projeto game mania usando o angular

---
+++ src/assets/js/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready (function() {
+    let slideAtual = 1;
+    let listaSlides = ["banner_f1", "banner_cs", "banner_lol"]
+    setInterval (mudarSlide, 2500)
+
+    function mudarSlide() {
+        console.log ("Slide atual: ", slideAtual);
+        
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual -1]) 
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass (listaSlides[ (listaSlides.length) -1 ])
+        }
+        
+        $("#carrossel").addClass (listaSlides[slideAtual]);
+
+        slideAtual++
+
+        if (slideAtual > (listaSlides.length-1) ) {
+            slideAtual = 0;
+        }
+    }
+})
+
+
+function mostrarMenu() {
+    let menu = document.getElementById ("menu")
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
